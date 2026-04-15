e75e5b9e0b56dcc6680dce93c7c3dc88f30297a5
Pawat Limpanuthai <pawat.limpanuthai@gmail.com>
2026-04-15 13:39:05 +0700

    - Updated : main.go call NewPostgresDB to test migrate DB.

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,26 @@
 package main
 
-func main() {
+import (
+	"log"
+	"os"
 
+	"github.com/fablelie/trello-clone-backend/internal/infrastructure/database"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// load Config from .env
+	if err := godotenv.Load(); err != nil {
+		log.Println("No .env file found, using system env")
+	}
+
+	_ = database.NewPostgresDB(
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	log.Println("Migration completed successfully!")
 }
